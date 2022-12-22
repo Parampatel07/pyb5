f608e6d23650f9afcb7275ee8e2abfdd602440e8
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-12-22 20:58:57 +0530

    file management

--- beautifulsoup/function.py
+++ beautifulsoup/function.py
@@ -11,3 +11,8 @@ def get_text_from_url(url):
 
 def replace_values(list_to_replace, item_to_replace, item_to_replace_with):
     return [item_to_replace_with if item == item_to_replace else item for item in list_to_replace]
+
+def divide_parts(name_of_list, size_of_part):
+    # looping till length l
+    for i in range(0, len(name_of_list), size_of_part):
+        yield name_of_list[i:i + size_of_part]
--- beautifulsoup/lesson4.py
+++ beautifulsoup/lesson4.py
@@ -1,4 +1,4 @@
-from function import get_text_from_url,replace_values
+from function import get_text_from_url,replace_values,divide_parts
 
 soup = get_text_from_url("""https://www.fifaratings.com/players""")
 # print(soup.prettify())
@@ -6,24 +6,24 @@ soup = get_text_from_url("""https://www.fifaratings.com/players""")
 data_html  = soup.find_all('div',{'class':'entries'})
 # print(data)
 
+data_text2=[]   
 for i in data_html:
     data_text = i.text
-    # print(data_text)
-data_text2=[]   
-# print(data_text)
-for letter in data_text:
-    if letter=='|':
-        letter=''
-    data_text2.append(letter)
-
-# print(data_text2)
-data_text3=''
-for i in data_text2:
-    data_text3 += i
-print(data_text3)
-data_text3 = data_text3.split(" ")
-# print(data_text3)
-data_text3=replace_values(data_text3,"",0)
-print(data_text3)
-data_text3=data_text3.remove(0)
-print(data_text3)
+    for letter in data_text:
+        if letter=='|':
+            letter=''
+        data_text2.append(letter)
+        data_text3=''
+        for i in data_text2:
+            data_text3 += i
+        # print(data_text3)
+        data_text3 = data_text3.split(" ")
+        data_text3=replace_values(data_text3,"",0)
+        # print(data_text3)
+        data_4=[]
+        for i in data_text3:
+            if isinstance(i,str)==True:
+                data_4.append(i)
+# print(data_4)
+answer=list(divide_parts(data_4,4))
+print(answer)
